b90fd97a801a4b9995887b1975f9cf62f6b8c3eb
Lai In Chang <lavender880331@gmail.com>
2022-12-08 17:22:12 +0100

    add different sound effect of gameover page

--- js/gameover.js
+++ js/gameover.js
@@ -12,13 +12,41 @@ function ranking(mark) {
     rank.id = "rank";
     if (mark < 20) {
         rank.innerHTML = `Er...You're a Zombie Newbie`;
+        gameoverSound1();
     } else if (mark < 40) {
         rank.innerHTML = `Aha! You're a Zombie Killer`;
+        gameoverSound2()
     } else if (mark < 60) {
         rank.innerHTML = `Hurray!! You're a Zombie Expert`;
+        gameoverSound3() 
     } else if (mark > 79) {
         rank.innerHTML = `Oh my Goodness!!! You're really a Zombie Master!!`;
+        gameoverSound4()
     }
+}
+
+function gameoverSound1() {
+    const sound = new Audio("../src/audio/level1.mp4")
+    sound.volume = 0.3;
+    sound.play();
+}
+
+function gameoverSound2() {
+    const sound = new Audio("../src/audio/level2.mp4")
+    sound.volume = 0.3;
+    sound.play();
+}
+
+function gameoverSound3() {
+    const sound = new Audio("../src/audio/level3.mp4")
+    sound.volume = 0.3;
+    sound.play();
+}
+
+function gameoverSound4() {
+    const sound = new Audio("../src/audio/level4.mp4")
+    sound.volume = 0.3;
+    sound.play();
 }
 
 score(mark);
